3d4eafca763c09e1b6f6fccd27b5d5c27e0f471d
Arendellian13 <listener1381@outlook.com>
2022-08-17 21:46:31 +0800

    修改谷歌统计

---
+++ scripts/google-analytics-injector.js
@@ -0,0 +1,12 @@
+hexo.extend.injector.register('head_begin',
+    `
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-RLRJSSD696"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-RLRJSSD696');
+</script>
+`)
